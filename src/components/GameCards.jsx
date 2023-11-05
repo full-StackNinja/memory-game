b40a431cd50678b99bc42fb7e37986a641054cc3
Imran Hussain <imranhussain399@gmail.com>
2023-11-05 16:05:03 +0500

    Add handleCardClick function to shufle cards randomly after clicking on any card

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -1,11 +1,26 @@
 import PropTypes from "prop-types";
-function GameCards({ cards }) {
+function GameCards({ cards, setCards }) {
+  const handleCardClick = (card) => {
+    const prevCards = [...cards];
+    prevCards.map((card, index) => {
+      const i = prevCards.length - 1 - index;
+      const j = Math.floor(Math.random() * (i + 1));
+      [prevCards[i], prevCards[j]] = [prevCards[j], prevCards[i]];
+    });
+    setCards(prevCards);
+  };
   return cards.map((card, index) => {
     console.log("card", card.title);
     const url = card.images["480w_still"].url;
     const title = card.title;
     return (
-      <div className="card-container" key={index}>
+      <div
+        className="card-container"
+        key={index}
+        onClick={() => {
+          handleCardClick(card);
+        }}
+      >
         <img className="card-image" src={url} alt={title} />
         <p className="card-title">{title}</p>
       </div>
