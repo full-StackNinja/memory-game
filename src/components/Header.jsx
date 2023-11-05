762303e1be2c0930b2d1e51e1d11ce016eb77ba6
Imran Hussain <imranhussain399@gmail.com>
2023-11-05 22:23:23 +0500

    Define Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="game-name">Memory Game</h1>
+      <p className="game-instruction">Hit any card only once otherwise you will lose!</p>
+    </div>
+  );
+};
+
+export default Header;
