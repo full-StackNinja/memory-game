8554b95ffe18e524cb299ce380f2ff0de726655e
Imran Hussain <imranhussain399@gmail.com>
2023-11-05 21:51:14 +0500

    Define handleCardClick function

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -11,13 +11,31 @@ function GameCards({
   setBestScore,
 }) {
   const handleCardClick = (card) => {
-    const prevCards = [...cards];
-    prevCards.map((card, index) => {
-      const i = prevCards.length - 1 - index;
-      const j = Math.floor(Math.random() * (i + 1));
-      [prevCards[i], prevCards[j]] = [prevCards[j], prevCards[i]];
-    });
-    setCards(prevCards);
+    if (cardClicks[card.id] === 0) {
+      const newCardClicks = { ...cardClicks };
+      newCardClicks[card.id] = 1;
+      setCardClicks(newCardClicks);
+      setTotalClicks(totalClicks + 1);
+
+      const prevCards = [...cards];
+      prevCards.map((card, index) => {
+        const i = prevCards.length - 1 - index;
+        const j = Math.floor(Math.random() * (i + 1));
+        [prevCards[i], prevCards[j]] = [prevCards[j], prevCards[i]];
+      });
+      setCards(prevCards);
+    } else {
+      if (totalClicks > bestScore) {
+        setBestScore(totalClicks);
+      }
+      setTotalClicks(0);
+      const oldCardClicks = { ...cardClicks };
+      const ids = Object.keys(oldCardClicks);
+      ids.forEach((cardId) => {
+        oldCardClicks[cardId] = 0;
+      });
+      setCardClicks(oldCardClicks);
+    }
   };
   return cards.map((card) => {
     // console.log("card", card.title);
