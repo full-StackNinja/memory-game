277787833c72fa54053d0153bf0fe8ffe14e6600
Imran Hussain <imranhussain399@gmail.com>
2023-11-05 14:38:05 +0500

    Add App component to return game Cards

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./styles/meyer-reset-1st.css";
+import "./styles/my-css-reset-2nd.css";
+import "./styles/normalize-3rd.css";
+import "./styles/typography-4th.css";
+import "./styles/App.css";
+import GameCards from "./components/GameCards";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const API_KEY = "CEmNnc4hkcJ7ocikD6uihaUTPMb8RPRE";
+  const baseUrl = "https://api.giphy.com/v2/emoji?";
+  const limit = 10;
+  const offset = 0;
+
+  useEffect(() => {
+    fetch(`${baseUrl}api_key=${API_KEY}&limit=${limit}&offset=${offset}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Data not fetched successfully!");
+        }
+      })
+      .then((result) => {
+        setCards(result.data);
+      });
+  }, []);
+  return (
+    <div className="cards-container">
+      <GameCards cards={cards} />
+    </div>
+  );
+}
+
+export default App;
